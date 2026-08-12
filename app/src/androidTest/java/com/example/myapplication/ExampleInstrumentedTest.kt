1a5d5fca104539c6cc7794c57ad042e6545221c5
Gregory Gapuz <gvgapuz82675@liceo.edu.ph>
2026-08-12 10:25:34 +0800

    Lab Activity 8: MVVM user profile app

--- app/src/androidTest/java/com/example/myapplication/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/example/myapplication/ExampleInstrumentedTest.kt
@@ -14,10 +14,9 @@ import org.junit.Assert.*
  * See [testing documentation](http://d.android.com/tools/testing).
  */
 @RunWith(AndroidJUnit4::class)
-class ExampleInstrumentedTest {
+class cdExampleInstrumentedTest {
     @Test
     fun useAppContext() {
-        // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
         assertEquals("com.example.myapplication", appContext.packageName)
     }
